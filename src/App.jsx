e8252ab02c4b7a104b3207f1d1e3c5db1ab9a2bd
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-17 23:02:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-center text-3xl font-bold text-purple-600'>Hello Jishan </h1>
+      
+    </>
+  )
+}
+
+export default App
