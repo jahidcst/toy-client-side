09b9f973c126cbb355d6e096d57539b0856a8b7e
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-20 01:19:12 +0600

    section2 added

---
+++ src/Pages/Home/ExtraSection/Section2.jsx
@@ -0,0 +1,35 @@
+
+
+const Section2 = () => {
+    return (
+        <div className="text-center mt-20">
+            <div className="mb-5">
+                <h1 className="text-5xl mb-3 font-bold">New Collection</h1>
+                <p className="text-xl font-bold">Customers’ favorite products this week. What’s so special?</p>
+            </div>
+            <div className="carousel carousel-center  p-4 space-x-4 bg-neutral rounded-box ml-5 mr-5">
+                <div className="carousel-item card">
+                    <img src="https://images.unsplash.com/photo-1609296583108-3a22c6748745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" className="rounded-box" />
+                    
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1517232202491-70f954c23ed4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1643921160105-b36dbe92ce87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1601956409343-a61004fff5cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1599744249842-ada1047fd47e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1596146827961-3b08fbef1ab1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80" className="rounded-box" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Section2;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Section1 from "../ExtraSection/Section1";
+import Section2 from "../ExtraSection/Section2";
 import Gallery from "../Gallery/Gallery";
 
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <Section1></Section1>
+            <Section2></Section2>
         </div>
     );
 };
