02095405bb0e352c6b7045432bbee5b32277e753
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-21 20:05:34 +0600

    handleDelete

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
+import ToysRow from "./ToysRow";
+import Swal from "sweetalert2";
 
 
 const MyToys = () => {
@@ -17,20 +19,52 @@ const MyToys = () => {
                 setToys(data)
             })
     }, [user]);
+
+    const handleDelete = id => {
+        fetch(`http://localhost:7000/allToys/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+                    const remaining = toys.filter(toy => toy._id !== id)
+                    setToys(remaining)
+                }
+            })
+    }
+
     return (
         <div>
             <h1>My ALL JOBS: {toys.length}</h1>
             <div className="form-control">
                 <div className="input-group">
-                    <input type="text" onChange={(e) => setSearch(e.target.value) } placeholder="Search…" className="input input-bordered" /> {" "}
+                    <input type="text" onChange={(e) => setSearch(e.target.value)} placeholder="Search…" className="input input-bordered" /> {" "}
                     <button className="btn btn-square">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
                 </div>
             </div>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <table className="w-full text-sm text-left text-white dark:text-gray-400">
+                    <thead className="text-lg text-green-400 uppercase bg-blue-950 dark:text-white">
                         <tr>
                             <th scope="col" className="px-6 py-3">
                                 Toy name
@@ -51,25 +85,11 @@ const MyToys = () => {
                     </thead>
                     <tbody>
                         {
-                            toys.map((toy) => {
-                                <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {toy.name}
-                                    </th>
-                                    <td className="px-6 py-4">
-                                        {toy.sub_category}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {toy.price}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {toy.quantity}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <button><a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:bg-gray">Delete</a></button>
-                                    </td>
-                                </tr>
-                            })
+                            toys.map((toy) => <ToysRow
+                                key={toy._id}
+                                toy={toy}
+                                handleDelete={handleDelete}
+                            ></ToysRow>)
                         }
 
                     </tbody>
